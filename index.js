9aae8ae0a62e98f56494e1991dab5f17bf80905d
NJUCO <122616369+NESTLEJUCO@users.noreply.github.com>
2023-08-14 21:24:01 -0600

    Code and pages updates
    Code and pages updates

--- index.js
+++ index.js
@@ -14,20 +14,40 @@ const bodyParser = require("body-parser"); //Read Incoming POST Request
 const fs = require("fs"); //File System
 const app = express();
 
-app.use(bodyParser.json()); //JSON Middleware
+app.use(bodyParser.json()); //JSON Middleware or Connection String
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/login.html");
+  res.sendFile(__dirname + "public/login.html");
 });
 
+//User registration
 let users = [];
 if (fs.existsSync("users.json")) {
   let data = fs.readFileSync("users.json", "utf-8");
   users = JSON.parse(data);
 }
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+// app.post("/register", (req, res) => {
+//   users.push(req.body);
+
+//   fs.writeFileSync("users.json", JSON.stringify(users));
+//   const data = req.body;
+
+//   res.send(
+//     `<center>
+//     <h2>Account Registration Successful!</h2>
+//     <p>${"Thank you! " + data.firstname + " " + data.lastname}</p>
+//     <p>Click to <a href="http://localhost:8081/">Login</a>.</p>
+//     </center>`
+//   );
+// });
 
 app.get("/users", (req, res) => {
   res.send(users);
@@ -48,6 +68,58 @@ app.post("/register", (req, res) => {
   );
 });
 
+//Property registration
+let property = [];
+if (fs.existsSync("property.json")) {
+  let propdata = fs.readFileSync("property.json", "utf-8");
+  property = JSON.parse(propdata);
+}
+
+app.get("/property", (req, res) => {
+  res.send(property);
+});
+
+app.post("/addProperty", (req, res) => {
+  property.push(req.body);
+
+  fs.writeFileSync("property.json", JSON.stringify(property));
+  const propdata = req.body;
+
+  res.send(
+    `<center>
+    <h2>Property Listing Successful!</h2>
+    <p>Click to <a href="http://localhost:8081/owneradmin.html">List Property / Workspace</a>.</p>
+    </center>`
+  );
+});
+
+//Workspace registration
+let workspace = [];
+if (fs.existsSync("workspace.json")) {
+  let wpdata = fs.readFileSync("workspace.json", "utf-8");
+  workspace = JSON.parse(wpdata);
+}
+
+app.get("/workspace", (req, res) => {
+  res.send(workspace);
+});
+
+app.post("/addWorkSpace", (req, res) => {
+  workspace.push(req.body);
+
+  fs.writeFileSync("workspace.json", JSON.stringify(workspace));
+  const wpdata = req.body;
+
+  res.send(
+    `<center>
+    <h2>Workspace Listing Successful!</h2>
+    <p>Click to <a href="http://localhost:8081/owneradmin.html">List Property / Workspace</a>.</p>
+    </center>`
+  );
+});
+
+//Login Validation
+
 //Routing and URL Forwarding
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
@@ -60,7 +132,7 @@ app.post("/login", (req, res) => {
     if (user.status === "owner") {
       res.redirect("/owneradmin.html");
     } else if (user.status === "coworker") {
-      res.redirect("/coworker.html");
+      res.redirect("/coworkerview.html");
     } else {
       // Handle other roles or scenarios if needed
       res.redirect("/");
